8c6f28b029394d02c6d4cfc2c32a3e3ab40d707e
EvaFD90 <evafilgueirasaxo@gmail.com>
2022-10-28 12:36:50 +0200

    init commit

---
+++ numbers.js
@@ -0,0 +1,11 @@
+let altura_cm = 168;
+let altura_m = 1.68;
+
+let peso = 70.3;
+
+let redondeoAltura = Math.ceil(altura_m);
+let redondeoPeso = Math.floor(peso);
+
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(sonIguales);
